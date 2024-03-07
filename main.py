494f176d080df297dab77e55fc112c78c3887af5
Arish <arishchowdhury.2021@gmail.com>
2024-03-07 23:22:58 +0600

    I have written code for inheritance.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 class Student:
+    def __init__(self, name):
+        self.name = name
     def say_hello(self):
-        print("hello")
+        print("hello ", self.name)
 
-Student().say_hello()
+class Polygon:
+
+    def __init__(self, sides):
+        self.sides = sides
+
+    def perimeter(self):
+        perimeter = sum(self.sides)
+        return perimeter
+
+if __name__ == '__main__':
+
+    Student("Arish").say_hello()
+
+    #Student1  is the instance variable
+    student1  = Student("Joe")
+    student1.say_hello()
+
+    polygon1 = Polygon(sides=[5,3,2])
+    print(polygon1.perimeter())
+    polygon2 = Polygon(sides=[7,9,13,11,17])
+    print(polygon2.perimeter())
